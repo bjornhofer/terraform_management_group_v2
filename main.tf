31c81a0a04bf6e19e2fb451a8580c3cbad8cd8ce
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-05-23 18:04:48 +0200

    First approach

--- main.tf
+++ main.tf
@@ -6,63 +6,55 @@ locals {
 }
 
 locals {
-    layer0 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[0], [])]))
-    layer1 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[1], [])]))
-    layer2 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[2], [])]))
-    layer3 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[3], [])]))
-    layer4 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[4], [])]))
-    layer5 = distinct(flatten([for layer1 in local.folders : try(split("/", layer1)[5], [])]))
-}
-/*
-output "folderstructure" {
-  value = local.folderstructure
+    // Management Group Settings
+    management_group_config_file_name = "management_group.json"
+    mgmt_grp_layer0_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 1] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
+    mgmt_grp_layer1_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 2] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
+    mgmt_grp_layer2_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 3] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
+    mgmt_grp_layer3_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 4] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
+    mgmt_grp_layer4_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 5] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
+    mgmt_grp_layer5_settings = {for folder in [for layers in local.folders : layers  if length(split("/", layers)) == 6] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {})}
 }
 
-output "folders" {
-  value = local.folders
-} 
-
-output "layer0" {
-  value = local.layer0
-}
-
-
-output "layer1" {
-  value = local.layer1
-}
-
-output "layer2" {
-  value = local.layer2
-}
-
-output "layer3" {
-  value = local.layer3
-}
-output "layer4" {
-  value = local.layer4
-}
-
-output "layer5" {
-  value = local.layer5
-}
-*/
-
-// Management Group
-/*
-resource "azurerm_management_group" "management_group" {
-  for_each = { for layer0 in local.layer0 : layer0 => layer0 }
-  name     = each.value
-  display_name = each.value
-  parent_management_group_id = var.parent_management_group_id
-}
-*/
-
-//for_each = { for layer0 in local.layer0 : layer0 => layer0 }
 locals {
-  layer0_setting = jsondecode(file("${each.value}management_group.json"))
+  layer0_parent = distinct([for parents in local.mgmt_grp_layer0_settings : parents.parent if contains(keys(parents), "parent")])
+  mgmt_layer0_parent_config = {for settings in local.mgmt_grp_layer0_settings : settings.name => settings.parent if contains(keys(settings), "parent")}
 }
 
-resource "azurerm_management_group" "management_group" {
-  for_each = local.layer0_setting
-  name = each.name
+output "test" {
+  value = local.layer0_parent
+}
+
+
+data "azurerm_management_group" "parent_layer0" {
+  //for_each = {for parents in distinct([for parents in local.mgmt_grp_layer0_settings : parents.parent if contains(keys(parents), "parent")]) : parents => parents}
+  for_each = local.mgmt_layer0_parent_config
+  name = each.value
+}
+
+
+resource "azurerm_management_group" "layer0_parent" {
+  for_each = local.mgmt_layer0_parent_config
+  name     = each.key
+  display_name = each.key
+  parent_management_group_id = data.azurerm_management_group.parent_layer0[each.value].id
+}
+
+resource "azurerm_management_group" "layer0_no_parent" {
+  for_each = local.mgmt_grp_layer0_settings
+  display_name = each.value.name
+  name = each.value.name
+}
+
+data "azurerm_management_group" "layer1" {
+  for_each = local.mgmt_grp_layer1_settings
+  name = each.value.parent
+}
+
+resource "azurerm_management_group" "layer1" {
+  for_each = local.mgmt_grp_layer1_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = data.azurerm_management_group.layer1[each.value.parent].id
+  depends_on = [ azurerm_management_group.layer0_no_parent, azurerm_management_group.layer0_parent ]
 }
