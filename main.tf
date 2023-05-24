35cffce8904b28b0be1029418e014a43df8461a6
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-05-24 12:49:02 +0200

    First stable

--- main.tf
+++ main.tf
@@ -21,17 +21,10 @@ locals {
   mgmt_layer0_parent_config = {for settings in local.mgmt_grp_layer0_settings : settings.name => settings.parent if contains(keys(settings), "parent")}
 }
 
-output "test" {
-  value = local.layer0_parent
-}
-
-
 data "azurerm_management_group" "parent_layer0" {
-  //for_each = {for parents in distinct([for parents in local.mgmt_grp_layer0_settings : parents.parent if contains(keys(parents), "parent")]) : parents => parents}
   for_each = local.mgmt_layer0_parent_config
   name = each.value
 }
-
 
 resource "azurerm_management_group" "layer0_parent" {
   for_each = local.mgmt_layer0_parent_config
@@ -46,15 +39,41 @@ resource "azurerm_management_group" "layer0_no_parent" {
   name = each.value.name
 }
 
-data "azurerm_management_group" "layer1" {
-  for_each = local.mgmt_grp_layer1_settings
-  name = each.value.parent
-}
-
 resource "azurerm_management_group" "layer1" {
   for_each = local.mgmt_grp_layer1_settings
   display_name = each.value.name
   name = each.value.name
-  parent_management_group_id = data.azurerm_management_group.layer1[each.value.parent].id
+  parent_management_group_id = length(local.layer0_parent) > 0 ? azurerm_management_group.layer0_parent[each.value.parent].id : azurerm_management_group.layer0_no_parent[each.value.parent].id
   depends_on = [ azurerm_management_group.layer0_no_parent, azurerm_management_group.layer0_parent ]
+}
+
+resource "azurerm_management_group" "layer2" {
+  for_each = local.mgmt_grp_layer2_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer1["${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == each.value.name][0]}"].id
+}
+
+resource "azurerm_management_group" "layer3" {
+  for_each = local.mgmt_grp_layer3_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer2["${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == each.value.name][0]}"].id
+  depends_on = [ azurerm_management_group.layer2 ]
+}
+
+resource "azurerm_management_group" "layer4" {
+  for_each = local.mgmt_grp_layer4_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer3["${[for layer4parent in local.mgmt_grp_layer4_settings : "${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == layer4parent.parent][0]}/${layer4parent.parent}" if layer4parent.name == each.value.name][0]}"].id
+  depends_on = [ azurerm_management_group.layer3 ]
+}
+
+resource "azurerm_management_group" "layer5" {
+  for_each = local.mgmt_grp_layer5_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer4["${[for layer5parent in local.mgmt_grp_layer5_settings : "${[for layer4parent in local.mgmt_grp_layer4_settings : "${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == layer4parent.parent][0]}/${layer4parent.parent}" if layer4parent.name == layer5parent.parent][0]}/${layer5parent.parent}" if layer5parent.name == each.value.name][0]}"].id
+  depends_on = [ azurerm_management_group.layer4 ]
 }
