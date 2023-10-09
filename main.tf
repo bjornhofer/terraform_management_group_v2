4efa763c1ea8259a1ba3275328b24878b9637ffa
bjornhofer@microsoft.com <bjornhofer@microsoft.com>
2023-10-09 15:46:46 +0000

    tidy up design (removed policy)

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
-// Management Group Settings
-variable "deploy_policies" {
-  type        = bool
-  default     = false
-  description = "set to true, this will deploy policies - set to false, it will only download the policies (default: false)"
-}
-
 // Main Settings
 locals {
   management_group_config_file_name = "management_group.json"
-  management_group_policy_file_name = "policies.json"
 }
 
 locals {
@@ -23,7 +15,92 @@ locals {
 }
 
 locals {
-  folderstructure_policies = [for folder in fileset(path.module, "**") : folder
-  if(length(regexall("(\\.json)", folder)) > 0)]
-  folders_policies = distinct(flatten([for folder in local.folderstructure_policies : join("/", slice(split("/", folder), 0, length(split("/", folder)) - 1))]))
+  // Management Group Settings
+  mgmt_grp_layer0_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 1] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
+  mgmt_grp_layer1_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 2] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
+  mgmt_grp_layer2_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 3] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
+  mgmt_grp_layer3_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 4] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
+  mgmt_grp_layer4_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 5] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
+  mgmt_grp_layer5_settings          = { for folder in [for layers in local.folders : layers if length(split("/", layers)) == 6] : folder => try(jsondecode(file("${path.module}/${folder}/${local.management_group_config_file_name}")), {}) }
 }
+
+locals {
+  layer0_existing      = [for management_group in local.mgmt_grp_layer0_settings : management_group.name if contains(keys(management_group), "existing") && management_group.existing == true]
+  layer0_create_parent = { for management_group in local.mgmt_grp_layer0_settings : management_group.name => management_group.parent if contains(keys(management_group), "parent") && !contains(local.layer0_existing, management_group.name) }
+  layer0_create        = [for management_group in local.mgmt_grp_layer0_settings : management_group.name if !contains(keys(management_group), "parent") && !contains(local.layer0_existing, management_group.name)]
+}
+
+// Lookup existing management groups
+data "azurerm_management_group" "layer0_existing" {
+  for_each = { for management_group in local.layer0_existing : management_group => management_group }
+  name     = each.key
+}
+
+// Create new management groups
+resource "azurerm_management_group" "layer0_create_parent" {
+  for_each                   = local.layer0_create_parent
+  name                       = each.key
+  display_name               = each.key
+  parent_management_group_id = data.azurerm_management_group.layer0_existing[each.value].id
+}
+
+resource "azurerm_management_group" "layer0_create" {
+  for_each     = { for management_group in local.layer0_create : management_group => management_group }
+  name         = each.key
+  display_name = each.key
+}
+
+// Merge management group information
+locals {
+  layer0 = merge(data.azurerm_management_group.layer0_existing, azurerm_management_group.layer0_create_parent, azurerm_management_group.layer0_create)
+}
+
+
+output "layer1_settings" {
+  value = local.mgmt_grp_layer1_settings
+}
+
+resource "azurerm_management_group" "layer1" {
+  for_each                   = local.mgmt_grp_layer1_settings
+  display_name               = each.value.name
+  name                       = each.value.name
+  parent_management_group_id = local.layer0[each.value.parent].id
+  depends_on                 = [azurerm_management_group.layer0_create_parent, azurerm_management_group.layer0_create]
+}
+
+resource "azurerm_management_group" "layer2" {
+  for_each                   = local.mgmt_grp_layer2_settings
+  display_name               = each.value.name
+  name                       = each.value.name
+  parent_management_group_id = azurerm_management_group.layer1["${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == each.value.name][0]}"].id
+  depends_on                 = [azurerm_management_group.layer1]
+}
+
+resource "azurerm_management_group" "layer3" {
+  for_each                   = local.mgmt_grp_layer3_settings
+  display_name               = each.value.name
+  name                       = each.value.name
+  parent_management_group_id = azurerm_management_group.layer2["${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == each.value.name][0]}"].id
+  depends_on                 = [azurerm_management_group.layer2]
+}
+
+//Uncomment if a layer 4 management group is added to the file structure
+/*
+  resource "azurerm_management_group" "layer4" {
+  for_each = local.mgmt_grp_layer4_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer3["${[for layer4parent in local.mgmt_grp_layer4_settings : "${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == layer4parent.parent][0]}/${layer4parent.parent}" if layer4parent.name == each.value.name][0]}"].id
+  depends_on = [ azurerm_management_group.layer3 ]
+}
+*/
+
+//Uncomment if a layer 5 management group is added to the file structure
+/*
+resource "azurerm_management_group" "layer5" {
+  for_each = local.mgmt_grp_layer5_settings
+  display_name = each.value.name
+  name = each.value.name
+  parent_management_group_id = azurerm_management_group.layer4["${[for layer5parent in local.mgmt_grp_layer5_settings : "${[for layer4parent in local.mgmt_grp_layer4_settings : "${[for layer3parent in local.mgmt_grp_layer3_settings : "${[for layer2parent in local.mgmt_grp_layer2_settings : "${[for layer1parent in local.mgmt_grp_layer1_settings : layer1parent.parent if layer1parent.name == layer2parent.parent][0]}/${layer2parent.parent}" if layer2parent.name == layer3parent.parent][0]}/${layer3parent.parent}" if layer3parent.name == layer4parent.parent][0]}/${layer4parent.parent}" if layer4parent.name == layer5parent.parent][0]}/${layer5parent.parent}" if layer5parent.name == each.value.name][0]}"].id
+  depends_on = [ azurerm_management_group.layer4 ]
+} */
--- provider.tf
+++ provider.tf
@@ -1,19 +1,12 @@
-provider "azurerm" {
-   features {}
-}
-
 terraform {
-  required_providers {
-    azuredevops = {
-      source = "microsoft/azuredevops"
-      version = ">=0.1.0"
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.55.0"
     }
   }
 }
 
-provider "azuredevops" {
-  # version = ">= 0.5.0"
-  # Remember to specify the org service url and personal access token details below
-  org_service_url       = local.org_service_url
-  personal_access_token = local.git_pat
+provider "azurerm" {
+    features {}
 }
